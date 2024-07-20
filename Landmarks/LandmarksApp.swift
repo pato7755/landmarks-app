e2799baf3f9e83d8e01b6f14255b62b1f2df7a4f
Patrick Essiam <pat.essiam701@gmail.com>
2024-07-20 18:33:46 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Patrick Essiam on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
