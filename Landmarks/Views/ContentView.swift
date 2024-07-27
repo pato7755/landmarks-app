45c7b9342e19f9bb000f33c15b46b563ac352366
Patrick Essiam <pat.essiam701@gmail.com>
2024-07-27 08:52:29 +0000

    Initial commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Patrick Essiam on 20/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack{
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                    
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
+            }.padding()
+            
+            Spacer()
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
